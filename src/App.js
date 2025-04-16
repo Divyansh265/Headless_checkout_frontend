01c2bd411bc9f0b684f0764656efe05f2f9abf49
Divyansh01-dev <divyansh.sharma@iamtechie.com>
2025-04-16 12:56:33 +0530

    test-3743

--- src/App.js
+++ src/App.js
@@ -1,68 +1,14 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Checkout from './Pages/Checkout';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Checkout from './Pages/Checkout';
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-
-//         <Route path="/cart-preview" element={<Checkout />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-
-const App = () => {
-  const [cartData, setCartData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const location = useLocation();
-  // const token = new URLSearchParams(location.search).get('token'); // Get token from query params
-  const token = `Z2NwLXVzLWVhc3QxOjAxSlJGRDlHRDcwRzQxNUhTQTZRUk02TTU1?key=6fff473dace8ec0a9a0d52a4329176f1`; // Get token from query params
-
-  useEffect(() => {
-    if (token) {
-      const fetchCartData = async () => {
-        try {
-          const response = await fetch(`https://headless-checkout-backend.onrender.com/api/cart-data?token=${token}`);
-          const data = await response.json();
-          setCartData(data);
-        } catch (error) {
-          console.error("Error fetching cart data:", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-
-      fetchCartData();
-    }
-  }, [token]);
-
-  if (loading) return <p>Loading cart...</p>;
-  if (!cartData) return <p>No cart found.</p>;
-
+function App() {
   return (
-    <div>
-      <h2>Checkout</h2>
-      <p><strong>Total:</strong> ${(cartData.total_price / 100).toFixed(2)}</p>
-      <ul>
-        {cartData.items.map(item => (
-          <li key={item.id}>
-            <img src={item.image} alt={item.title} width="50" />
-            <p>{item.title}</p>
-            <p>Qty: {item.quantity}</p>
-            <p>Price: ${(item.price / 100).toFixed(2)}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h1>h11</h1>
+      <Checkout />
+    </>
   );
-};
+}
 
 export default App;
