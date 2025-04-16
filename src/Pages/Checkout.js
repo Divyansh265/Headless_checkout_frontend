a7867e6cc55147d6cbc7b2cd280b5b7f4551fb0d
Divyansh01-dev <divyansh.sharma@iamtechie.com>
2025-04-16 17:08:58 +0530

    test-33

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -4,9 +4,9 @@ import { useLocation } from 'react-router-dom';
 const Checkout = () => {
     const [cartData, setCartData] = useState(null);
     const [loading, setLoading] = useState(true);
-    //   const location = useLocation();
-    //  const token = new URLSearchParams(location.search).get('token'); // Get token from query params
-    const token = `Z2NwLWFzaWEtc291dGhlYXN0MTowMUpSWjVURDFTNDNRN1dENE1CUllYNUtOMg?key=0036c02b5b3f3f442a7e2deda4eddd4b`;
+    const location = useLocation();
+    const token = new URLSearchParams(location.search).get('token'); // Get token from query params
+    // const token = `Z2NwLWFzaWEtc291dGhlYXN0MTowMUpSWjVURDFTNDNRN1dENE1CUllYNUtOMg?key=0036c02b5b3f3f442a7e2deda4eddd4b`;
     useEffect(() => {
         if (token) {
             const fetchCartData = async () => {
