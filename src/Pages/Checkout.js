368418d452154bd66de0af04fdf82092cd88b56d
Divyansh Sharma <divyansh.sharma@iamtechie.com>
2025-04-22 15:09:28 +0530

    new_update_1

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import './Checkout.css';
 const Checkout = () => {
     const [cartData, setCartData] = useState(null);
     const [loading, setLoading] = useState(true);
